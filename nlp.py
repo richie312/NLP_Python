018b5bd8689f71758fceaa596f8febe4b2d2e973
richie312 <richie.chatterjee31@gmail.com>
2019-01-05 10:41:42 +0530

    Added the initial files

---
+++ nlp.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  5 10:16:57 2019
+
+@author: Richie
+"""
+import nltk
+from nltk.book import *
+import os
+os.chdir(r"C:\Users\Richie\Desktop\NLP_Python")
+NLTK_Location = r"C:\Users\Richie\AppData\Roaming\nltk_data"
+nltk.download()
+
