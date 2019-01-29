e705587eb134a2d1806dbef36e95e3261feddcaa
Aritra Chatterjee <aritra.chatterjee@gspann.com>
2019-01-29 11:24:35 +0530

    Added the general image scrape file

--- image_webscrape.py
+++ image_webscrape.py
@@ -7,17 +7,40 @@ Created on Sun Jan 27 12:05:09 2019
 
 from bs4 import BeautifulSoup
 import requests
+import urllib
+import pandas as pd
 
-"""Accessing the image from web and from Disk"""
-url = "https://www.pyimagesearch.com/2017/08/21/deep-learning-with-opencv/"
+"""Accessing the image from web """
+url = "https://www.kohls.com/search.jsp?submit-search=web-ta-keyword-cat&search=floral+dresses&CN=Department%3AClothing%2BGender%3AWomens&S=1&PPP=60&kls_sbp=30791824592479704744051542053675060422"
 
 page = requests.get(url)
 raw_soup = BeautifulSoup(page.content,'html.parser')
 
-images = raw_soup.find_all('img')
+images = raw_soup.find_all('img',class_="pmp-hero-img")
+
+"""INstantitate the url and description list"""
+image_link =[]
+image_desc = []
 
 for image in images:
-    print("{0:20}{1:20}".format("Title","links"))
-    print(image.attrs.get('title'))
-    print(image.attrs.get('src'))
+    image_link.append(image.attrs.get('src'))
+    image_desc.append(image.attrs.get('title'))
+
+# =============================================================================
+# """Get the files to local drive"""
+# 
+# for i in range(len(image_desc)):
+#     urllib.request.urlretrieve(image_link[i], image_desc[i]+'.jpg')
+# 
+# =============================================================================
+"""Instantiate tthe dataframe"""
+
+dataframe = pd.DataFrame()
+
+dataframe["Image_Index"] = ["Image_{}.jpg".format(i) for i in range(len(image_desc))] 
+dataframe["features"] = [','.join(i.split()[1:]) for i in image_desc]
+
+""" Write the submission file"""
+
+dataframe.to_csv("kohl_features.csv",index = False)
 
